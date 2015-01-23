97b81b3e1774c96b1fd629494a3b69328dfdbf34
Mattis Kancans Envall <mattiskan@gmail.com>
2015-01-23 21:54:25 +0100

    pre thread

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -3,7 +3,7 @@ import bs4
 import urllib.request
 import pickle
 import os.path
-
+import re
 
 def parse_posts(link):
     url = "%s%s" % ("http://www.familjeliv.se", link)
@@ -20,21 +20,44 @@ def parse_post(url):
         webpage = urllib.request.urlopen(url)
         soup = bs4.BeautifulSoup(webpage.read().decode('utf8'))
     except UnicodeDecodeError:
-        print("fail")
+        visited.add(url)
+        print("failed", url)
         return False
 
-    return [ anchor.getText()
-             for anchor in soup.find_all('div', 'message-body') ]
+    return " ".join([ anchor.getText()
+             for anchor in soup.find_all('div', 'message-body') ])
 
 
 def write_post(post_data, url):
     filename = "posts/%s" % url.split("/")[-1]
 
     with open(filename, 'w') as output:
-        output.write(" ".join(post_data).replace("\n", " "))
+        output.write(tokenize(post_data))
 
     visited.add(url)
     print("added", url)
+
+def tokenize(data):
+    data = data.replace("\n", " ").lower()
+
+    data = data.replace("-", "")
+    data = data.replace("(", "")
+    data = data.replace(")", "")    
+    
+    data = re.split('[.-:;?!]+', data)
+
+    sent = [ sent.split(" ") for sent in data ]
+
+    ret = []
+    for words in sent:
+        next = [ word for word in words if check(word)]
+        if next:
+            ret += [ next ]
+
+    return repr(ret)
+
+def check(word):
+    return word and word != '-'
 
 forum_prefix="http://www.familjeliv.se/forum"
 def parse_forum_page(fid, sidnummer):
@@ -52,10 +75,15 @@ def parse_forum(fid):
     links = parse_forum_page(fid, 1)
 
     while (links != []):
+        page = "pagenum %d %d" % (fid, pagenum)
 
-        [ parse_posts(link) for link in links ]
-        [ parse_posts(link) for link in links ]
+        if page in visited:
+            pagenum += 1
+            continue
         
+        [ parse_posts(link) for link in links ]
+
+        visited.add(page)
         pagenum += 1
         links = parse_forum_page(fid, pagenum)
 
@@ -78,7 +106,9 @@ if __name__ == "__main__":
     visited = read_visited()
     
     try:
-        parse_forum(1)
+        parse_forum(4)
+    except KeyboardInterrupt:
+        pass
     finally:
         print("wrote visited")
         write_visited()
